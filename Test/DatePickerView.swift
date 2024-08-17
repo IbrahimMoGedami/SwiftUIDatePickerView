823dbd61c4b268c44a686383add043e0e89e3e80
Ibrahim Mo Gedami <hemagedamy@gmail.com>
2024-08-17 15:31:45 +0400

    commit

--- Test/DatePickerView.swift
+++ Test/DatePickerView.swift
@@ -1,137 +1,128 @@
-//
-//  DatePickerView.swift
-//  Test
-//
-//  Created by Ibrahim Gedami on 17/08/2024.
-//
-
 import SwiftUI
 
-struct DatePickerView: View {
-    
-    var components: DatePickerComponents = [.date, .hourAndMinute]
-    @Binding var date: Date
-    var formattedString: (Date) -> String
-    @State private var viewId = UUID().uuidString
-    @FocusState var isActive
-    
-    var body: some View {
-        TextField(viewId, text: .constant(formattedString(date)))
-            .focused($isActive)
-            .toolbar {
-                ToolbarItem(placement: .keyboard) {
-                    Button {
-                        isActive = false
-                    } label: {
-                        Text("Done")
-                        
-                    }
-                    .tint(.primary)
-                    .frame(maxWidth: .infinity, alignment: .trailing)
-                    
-                }
-            }
-            .overlay {
-                AddInputViewToDatePickerView(id: viewId) {
-                    DatePicker("", selection: $date, displayedComponents: components)
-                        .labelsHidden()
-                        .datePickerStyle(.wheel)
-                }
-            }
-            .onTapGesture {
-                isActive = true
-            }
-    }
-    
-}
-
 struct ContentView: View {
-    
-    @State var date: Date = .now
-    
+    @State var selectedIndex: Int? = nil
+    let options: [String] = ["Option1", "Option2"]
+
     var body: some View {
-        NavigationStack {
-            DatePickerView(date: $date) { selectedDate in
-                let date = selectedDate.prettyShortDate
-                return date ?? ""
-            }
-        }
+       PickerField("Select an option", data: self.options, selectionIndex: self.$selectedIndex)
     }
-    
 }
 
-#Preview {
-    ContentView()
-}
+struct PickerField: UIViewRepresentable {
+    // MARK: - Public properties
+    @Binding var selectionIndex: Int?
 
-fileprivate struct AddInputViewToDatePickerView<Content: View>: UIViewRepresentable {
-    
-    var id: String
-    @ViewBuilder var content: Content
-    
-    func makeUIView(context: Context) -> some UIView {
-        let view = UIView()
-        view.backgroundColor = .clear
-        DispatchQueue.main.async {
-            if let window = view.window, let textField = window.allSubViews(type: UITextField.self).first(where: { $0.placeholder == id }) {
-                let hostView = UIHostingController(rootView: content).view
-                hostView?.backgroundColor = .clear
-                hostView?.frame.size =  hostView?.intrinsicContentSize ?? .zero
-                textField.inputView = hostView
-                textField.tintColor = .clear
-                textField.reloadInputViews()
-            }
-        }
-        return view
-    }
-    
-    func updateUIView(_ uiView: UIViewType, context: Context) {
-        
-    }
-    
-}
+    // MARK: - Initializers
+    init<S>(_ title: S, data: [String], selectionIndex: Binding<Int?>) where S: StringProtocol {
+        self.placeholder = String(title)
+        self.data = data
+        self._selectionIndex = selectionIndex
 
-fileprivate extension UIView {
-    
-    func allSubViews<T: UIView>(type: T.Type) -> [T] {
-        var resultViews = subviews.compactMap({ $0 as? T })
-        
-        for subView in subviews {
-            resultViews.append(contentsOf: subView.allSubViews(type: type))
-        }
-        
-        return resultViews
+        textField = PickerTextField(data: data, selectionIndex: selectionIndex)
     }
-    
-}
 
-extension Date {
-    
-    var prettyShortDate: String? {
-        let dateFormatter = DateFormatter()
-        dateFormatter.dateFormat = "dd-MM-yyyy"
-        let date = dateFormatter.string(from: self)
-        return date
+    // MARK: - Public methods
+    func makeUIView(context: UIViewRepresentableContext<PickerField>) -> UITextField {
+        textField.placeholder = placeholder
+        return textField
     }
-    
-}
 
-extension String {
-    
-    var date: Date? {
-        let dateFormatter = DateFormatter()
-        dateFormatter.dateFormat = "yyyy/MM/dd HH:mm:ss"
-        if let newDate = dateFormatter.date(from: self) {
-            return newDate
+    func updateUIView(_ uiView: UITextField, context: UIViewRepresentableContext<PickerField>) {
+        if let index = selectionIndex {
+            uiView.text = data[index]
         } else {
-            dateFormatter.dateFormat = "dd-MMM-yyyy HH:mm:ss"
-            if let newDate = dateFormatter.date(from: self) {
-                return newDate
-            } else {
-                dateFormatter.dateFormat = "dd-MM-yyyy HH:mm:ss"
-                return dateFormatter.date(from: self)
-            }
+            uiView.text = ""
         }
     }
+
+    // MARK: - Private properties
+    private var placeholder: String
+    private var data: [String]
+    private let textField: PickerTextField
+}
+
+class PickerTextField: UITextField {
+    // MARK: - Public properties
+    var data: [String]
+    @Binding var selectionIndex: Int?
+
+    // MARK: - Initializers
+    init(data: [String], selectionIndex: Binding<Int?>) {
+        self.data = data
+        self._selectionIndex = selectionIndex
+        super.init(frame: .zero)
+        
+        setupPickerView()
+        setupToolbar()
+        customizeAppearance()
+    }
+
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    // MARK: - Private properties
+    private lazy var pickerView: UIPickerView = {
+        let pickerView = UIPickerView()
+        pickerView.delegate = self
+        pickerView.dataSource = self
+        return pickerView
+    }()
+
+    private func setupPickerView() {
+        self.inputView = pickerView
+    }
     
+    private func setupToolbar() {
+        let toolbar = UIToolbar()
+        toolbar.sizeToFit()
+
+        let doneButton = UIBarButtonItem(title: "Done", style: .done, target: self, action: #selector(donePressed))
+        let flexibleSpace = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
+        let cancelButton = UIBarButtonItem(title: "Cancel", style: .plain, target: self, action: #selector(cancelPressed))
+        
+        toolbar.setItems([cancelButton, flexibleSpace, doneButton], animated: false)
+        self.inputAccessoryView = toolbar
+    }
+
+    private func customizeAppearance() {
+        self.tintColor = .clear
+        self.borderStyle = .roundedRect
+        self.backgroundColor = .white
+        self.textColor = .black
+        self.font = UIFont.systemFont(ofSize: 16)
+    }
+
+    // MARK: - Private methods
+    @objc
+    private func donePressed() {
+        self.selectionIndex = self.pickerView.selectedRow(inComponent: 0)
+        self.endEditing(true)
+    }
+    
+    @objc
+    private func cancelPressed() {
+        self.endEditing(true)
+    }
+}
+
+// MARK: - UIPickerViewDataSource & UIPickerViewDelegate extension
+extension PickerTextField: UIPickerViewDataSource, UIPickerViewDelegate {
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return self.data.count
+    }
+
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return self.data[row]
+    }
+
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        self.selectionIndex = row
+    }
 }
